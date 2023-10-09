b2188e3645af5b2ca22544bef25d637dcae14abf
thuraitunza <tt@za.com.mm>
2023-10-09 14:50:27 +0630

    Flipcart

---
+++ src/main.js
@@ -0,0 +1,235 @@
+const main_1 = document.querySelector("#main_1");
+
+const item_1 = document.querySelector("#item_1");
+const item_1_display = document.querySelector("#item_1_display");
+
+const item_2 = document.querySelector("#item_2");
+const item_2_display = document.querySelector("#item_2_display");
+
+const item_3 = document.querySelector("#item_3");
+const item_3_display = document.querySelector("#item_3_display");
+
+const item_4 = document.querySelector("#item_4");
+const item_4_display = document.querySelector("#item_4_display");
+
+const item_5 = document.querySelector("#item_5");
+const item_5_display = document.querySelector("#item_5_display");
+
+const item_6 = document.querySelector("#item_6");
+const item_6_display = document.querySelector("#item_6_display");
+
+const item_7 = document.querySelector("#item_7");
+const item_7_display = document.querySelector("#item_7_display");
+
+const item_8 = document.querySelector("#item_8");
+const item_8_display = document.querySelector("#item_8_display");
+
+const item_9 = document.querySelector("#item_9");
+const item_9_display = document.querySelector("#item_9_display");
+
+const item_10 = document.querySelector("#item_10");
+const item_10_display = document.querySelector("#item_10_display");
+
+const item_11 = document.querySelector("#item_11");
+const item_11_display = document.querySelector("#item_11_display");
+
+
+// Add a mouseover event listener
+
+
+item_1.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'block';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+});
+
+item_2.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'block';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+  });
+  
+// item_2.addEventListener('mouseout', () => {
+//       item_2_display.style.display = 'none';
+// });
+
+item_3.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'block';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+  });
+
+// item_3.addEventListener('mouseout', () => {
+//     item_3_display.style.display = 'none';
+// });
+
+item_4.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'block';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+  });
+
+// item_4.addEventListener('mouseout', () => {
+//     item_4_display.style.display = 'none';
+// });
+
+item_5.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'block';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+  });
+
+// item_5.addEventListener('mouseout', () => {
+//     item_5_display.style.display = 'none';
+// });
+
+item_6.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'block';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+  });
+
+// item_6.addEventListener('mouseout', () => {
+//     item_6_display.style.display = 'none';
+// });
+
+item_7.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'block';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+  });
+
+// item_7.addEventListener('mouseout', () => {
+//     item_7_display.style.display = 'none';
+// });
+
+item_8.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'block';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+  });
+
+// item_8.addEventListener('mouseout', () => {
+//     item_8_display.style.display = 'none';
+// });
+
+item_9.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'block';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'none';
+  });
+
+// item_9.addEventListener('mouseout', () => {
+//     item_9_display.style.display = 'none';
+// });
+
+item_10.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'block';
+    item_11_display.style.display = 'none';
+  });
+
+// item_10.addEventListener('mouseout', () => {
+//     item_10_display.style.display = 'none';
+// });
+
+item_11.addEventListener('mouseover', () => {
+    item_1_display.style.display = 'none';
+    item_2_display.style.display = 'none';
+    item_3_display.style.display = 'none';
+    item_4_display.style.display = 'none';
+    item_5_display.style.display = 'none';
+    item_6_display.style.display = 'none';
+    item_7_display.style.display = 'none';
+    item_8_display.style.display = 'none';
+    item_9_display.style.display = 'none';
+    item_10_display.style.display = 'none';
+    item_11_display.style.display = 'block';
+  });
+
+// item_11.addEventListener('mouseout', () => {
+//     item_11_display.style.display = 'none';
+// });
+
+
+
